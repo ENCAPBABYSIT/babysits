c09bf138ff67f7f42c751e905b990bc938900447
ENCAPBABYSIT <encapbabysits@gmail.com>
2024-08-05 13:02:42 -0500

    5 de agosto

--- rest_api_node__ts_server/src/config/db.ts
+++ rest_api_node__ts_server/src/config/db.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'sqlite'
+const sequelize = new Sequelize('dbwxlibrkaxkp9', 'unm3zcftonps6', 'jl2C@e`5h22%', {
+  host: '34.174.182.48',
+  dialect: 'mysql'
 });
 
 export const connectDB = async () => {
@@ -14,18 +14,3 @@ export const connectDB = async () => {
     console.error('Unable to connect to the database:', error);
   }
 }
-
-
-
-
-
-// import mysql from 'mysql2/promise';
-
-// const pool = mysql.createPool({
-//   host: '34.174.182.48',
-//   user: 'unm3zcftonps6',
-//   password: '$1_d+eoq8s52',
-//   database: 'dbwxlibrkaxkp9'
-// });
-
-// export default pool;
